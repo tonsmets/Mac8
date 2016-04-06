b1c83d5f8a5e80330be217bf2428e62358f32d44
Ton Smets <info@tonsmets.nl>
2016-04-06 16:59:44 +0200

    Initial Commit

---
+++ Mac8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mac8
+//
+//  Created by Ton Smets on 06-04-16.
+//  Copyright © 2016 Ton Smets. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
